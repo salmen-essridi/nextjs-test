a692ab46942148b84ad4b1812828470b9b80fea1
salmen-essridi <49515523+salmen-essridi@users.noreply.github.com>
2022-03-21 12:04:26 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
+  return (https://github.com/salmen-essridi/nextjs-test/blob/master/pages/index.js
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -20,7 +20,7 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="https://nextjs.org/docs   " className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
